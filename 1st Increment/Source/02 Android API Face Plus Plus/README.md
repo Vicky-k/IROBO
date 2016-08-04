ea81af53b667a80a580063dae71e7c2315ba67a2
Vikas-kondapalli <vikas.kondapalli94@gmail.com>
2016-08-03 23:17:39 -0700

    rearr

---
+++ 1st Increment/Source/02 Android API Face Plus Plus/README.md
@@ -0,0 +1,3 @@
+# Faceplusplus
+    An Android APP for face recognition~
+    Faceplusplus API:http://www.faceplusplus.com/
---
+++ 1st Increment/Source/02 Android API Face Plus Plus/app/src/main/java/com/example/chanst/facepp/Constant.java
@@ -0,0 +1,11 @@
+package com.example.chanst.facepp;
+
+/**
+ * Created by chanst on 16-2-14.
+ */
+public class Constant {
+    //public static final String KEY ="823f5fd784116538e34f8337c8a57aa8";
+    //public static final String SECRET ="QzGRnAUaK2hfZdFwkBhwOUISGt4HLvuI";
+    public static final String KEY ="3d7ee3617d5afbd9623c9bfb9271a9d7";
+    public static final String SECRET ="YULzlXK3F0se3DtGM74KuIFv6DVnsEsF";
+}
